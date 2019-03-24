acc56a01f93d922e36c4f5aedc4fdf3d2f44c238
jonah <wander.shadow.cat@gmail.com>
2019-03-24 22:40:43 +0800

    Lecture 41. Injecting MaxNumber and guessCount through property file within resources folder. @Value, and @PropertySource have been used.

--- core/src/main/java/mycode/learnprogramming/config/GameConfig.java
+++ core/src/main/java/mycode/learnprogramming/config/GameConfig.java
@@ -2,14 +2,20 @@ package mycode.learnprogramming.config;
 
 import mycode.learnprogramming.GuessCount;
 import mycode.learnprogramming.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
     // == fields ==
-    private int guessCount = 8;
-    private int maxNumber = 75;
+    @Value("${game.guessCount:999}")
+    private int guessCount;
+
+    @Value("${game.maxNumber:999}")
+    private int maxNumber;
 
     // == Beands Methods ==
     @Bean
