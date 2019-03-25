6bc00ffca342a74fc591bfe16f386d0d63c3f5bb
jonah <wander.shadow.cat@gmail.com>
2019-03-25 16:20:58 +0800

    Lombok plugins added

--- console/src/main/java/mycode/learnprogramming/console/Main.java
+++ console/src/main/java/mycode/learnprogramming/console/Main.java
@@ -1,14 +1,12 @@
 package mycode.learnprogramming.console;
 
+import lombok.extern.slf4j.Slf4j;
 import mycode.learnprogramming.config.GameConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@Slf4j
 public class Main {
-
-    private final static Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         //create context(container) with annotation configuration
