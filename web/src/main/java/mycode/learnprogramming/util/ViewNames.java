7d12624bf0a8c8cec42f162fb433c2c7153bf094
jonah <wander.shadow.cat@gmail.com>
2019-04-05 12:49:42 +0800

    Lecture 80 * Setup Thymeleaf and Initial Templates

---
+++ web/src/main/java/mycode/learnprogramming/util/ViewNames.java
@@ -0,0 +1,9 @@
+package mycode.learnprogramming.util;
+
+public final class ViewNames {
+    // == constants ==
+    public static final String HOME ="home";
+
+    // == constructor ==
+    private ViewNames(){}
+}
